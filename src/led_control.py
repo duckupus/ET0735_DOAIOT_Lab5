45cc782fd9f59a8b8652df24edeaa351269c4949
duckupus <75720972+duckupus@users.noreply.github.com>
2023-11-21 11:58:53 +0800

    add flashing LEDs

--- src/led_control.py
+++ src/led_control.py
@@ -4,3 +4,36 @@ from time import sleep
 
 from hal import hal_keypad as keypad
 
+global LED_control_state
+
+
+def set_control_state(x: int):
+    global LED_control_state
+    LED_control_state = x
+
+
+def led_thread():
+    global delay
+
+    delay = 1
+    while True:
+        if LED_control_state == 0:
+            led.set_output(20, 0)
+            continue
+        if delay != 0:
+            led.set_output(20, 1)
+            sleep(delay)
+            led.set_output(20, 0)
+            sleep(delay)
+
+
+def led_control_init():
+    global delay
+    led.init()
+    global LED_control_state
+    LED_control_state = 0
+
+    t1 = Thread(target=led_thread)
+
+    t1.start()
+    delay = 1
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from time import sleep
 from threading import Thread
 from hal import hal_keypad as keypad
 from hal import hal_lcd as LCD
+from led_control import led_control_init
 
 #Empty list to store sequence of keypad presses
 password = []
@@ -8,9 +10,21 @@ password = []
 lcd = LCD.lcd()
 lcd.lcd_clear()
 
+global LED_control_state
 #Call back function invoked when any key on keypad is pressed
 def key_pressed(key):
     password.append(key)
+    global LED_control_state
+    from led_control import set_control_state
+    lcd.lcd_clear()
+    lcd.lcd_display_string("LED Control", 1)
+    if key == 0:
+        LED_control_state = 0
+        lcd.lcd_display_string("OFF LED", 2)
+    elif key == 1:
+        LED_control_state = 1
+        lcd.lcd_display_string("Blink LED", 2)
+    set_control_state(LED_control_state)
 
     print(password)
 
@@ -19,9 +33,13 @@ def main():
     # Initialize LCD
     lcd = LCD.lcd()
     lcd.lcd_clear()
+    global LED_control_state
+    LED_control_state = 0
 
     # Display something on LCD
-    lcd.lcd_display_string("Lab 5", 1)
+    lcd.lcd_display_string("LED Control", 1)
+    lcd.lcd_display_string("0:Off1:Blink", 2)
+    sleep(1)
 
     # Initialize the HAL keypad driver
     keypad.init(key_pressed)
@@ -29,6 +47,7 @@ def main():
     # Start the keypad scanning which will run forever in an infinite while(True) loop in a new Thread "keypad_thread"
     keypad_thread = Thread(target=keypad.get_key)
     keypad_thread.start()
+    led_control_init()
 
 
 # Main entry point
